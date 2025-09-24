552ac6f6efa282331830a67b987b9b91540eab6e
leejinwoo <julee@vessellink.com>
2025-09-24 15:15:13 +0900

    어플리케이션 빈 등록 extension 분리

---
+++ TodoApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using TodoApi.Repositories;
+using TodoApi.Services;
+
+namespace TodoApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITodoItemRepository, TodoItemInmemoryRepository>();
+            services.AddScoped<TodoItemService>();
+            return services;
+        }
+    }
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,22 +1,20 @@
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Repositories;
-using TodoApi.Services;
+using TodoApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoApi.Models.TodoContext>(opt => 
+builder.Services.AddDbContext<TodoApi.Models.TodoContext>(opt =>
    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<ITodoItemRepository, TodoItemInmemoryRepository>();
-builder.Services.AddScoped<TodoItemService>();
 builder.Services.AddLogging();
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
